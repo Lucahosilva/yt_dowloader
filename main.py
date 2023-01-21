e8c5a83b96e474f453b33ed8c9179352fa26ce55
lucas silva <lucashosilva@gmail.com>
2023-01-20 23:05:45 -0300

    adicionando função baixar_playlist

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from pytube import YouTube, streams
-
-link = input('Insira o link ')
-yt = YouTube(link)
-ys = yt.streams.get_highest_resolution().download()
+from pytube import YouTube, streams, Playlist
 
 
+def baixar_video(link):
+
+    yt = YouTube(link)
+    print('baixando...')
+    ys = yt.streams.get_highest_resolution().download()
+    print('Sucesso.')
+
+
+def baixar_playlist(link):
+
+    yt = Playlist(link)
+    print('baixando...')
+    for url in yt:
+        video = YouTube(url)
+        stream = video.streams.get_highest_resolution()
+        stream.download(output_path='playlist')
+    print('Sucesso.')
+
+
+if __name__ == '__main__':
+
+    link = input('Insira o link ')
+    tipo = int(input(' digite 0 para video unico ou 1 pra playlist '))
+
+    if tipo == 0:
+        baixar_video(link)
+    else:
+        baixar_playlist(link)
